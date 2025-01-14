aa7e990bdbf69a7fc60900ae512c206bd93a61ef
RikitoIshizu <isizu69@gmail.com>
2025-01-14 19:40:15 +0900

    Revert "congigに余計なものを見ないようにする"
    This reverts commit 81c9a634476b010e0cff2964c3e5b9261d63c289.

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,18 +7,22 @@ export async function middleware(req: NextRequest) {
   const publicPaths = ['/login'];
 
   if (publicPaths.some((path) => pathname.startsWith(path))) {
+    console.log('パス', pathname);
+    console.log('公開ページへのアクセスです。ミドルウェアをスキップします。');
     return NextResponse.next();
   }
 
   const token = req.cookies.get('token')?.value;
 
   if (!token) {
-    return NextResponse.redirect(new URL('/login', req.url));
+    console.log('リダイレクトする');
+    //   return NextResponse.redirect(new URL('/login', req.url));
   }
 
+  // console.log('トークンが有効です。リクエストを続行します。');
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/((?!login|api|_next/static|favicon.ico).*)'],
+  matcher: ['/((?!login).*)'],
 };
