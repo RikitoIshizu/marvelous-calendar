f2fe2d6b8b41c9c6e6e9b29f11f4e1c566ee2428
RikitoIshizu <isizu69@gmail.com>
2025-01-16 23:05:32 +0900

    tailwind.config.js更新

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
-const colors = require("tailwindcss/colors");
+const colors = require('tailwindcss/colors');
 
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
-  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        teal: colors.teal,
-        hobby: {
-          100: "#001871",
-          200: "#ff585d",
-          300: "#ffb549",
-          400: "#41b6e6",
-        },
-      },
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./app/**/*.{js,jsx,ts,tsx}'],
+	purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+	darkMode: 'media', // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				teal: colors.teal,
+				hobby: {
+					100: '#001871',
+					200: '#ff585d',
+					300: '#ffb549',
+					400: '#41b6e6',
+				},
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 };
