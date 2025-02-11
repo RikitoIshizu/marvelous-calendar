6b7c2ccd25c17490771bacfee688f506cc2fe5eb
RikitoIshizu <isizu69@gmail.com>
2025-02-12 00:23:18 +0900

    いらないコメント削除

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 	plugins: [react()],
-	// Vitest の設定はここに含めることができます
 	test: {
-		globals: true, // Jest のようなグローバルなテスト API を使いたい場合
-		environment: 'jsdom', // DOM 操作のテストの場合は 'jsdom'、Node.js のテストなら 'node'
-		// その他の Vitest 固有の設定を必要に応じて追加
+		globals: true, // Jest のようなグローバルなテスト API を使いたいので
+		environment: 'jsdom',
 	},
 });
