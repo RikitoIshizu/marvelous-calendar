e78d1820a73760b7cd0a9ad21e8c460f595ff0df
RikitoIshizu <isizu69@gmail.com>
2025-10-29 21:48:44 +0900

    src/hooks/useAsyncLoading.tsを修正

--- src/hooks/useAsyncLoading.ts
+++ src/hooks/useAsyncLoading.ts
@@ -1,18 +1,28 @@
 import { useLoading } from 'context/LoadingContext';
+import { useCallback } from 'react';
 
-// eslint-disable-next-line no-unused-vars
-export const useAsyncLoading = <T>(func: (...args: any[]) => Promise<T>) => {
+export const useAsyncLoading = <T, Args extends any[]>(
+	func: (..._: Args) => Promise<T>,
+) => {
 	const { setIsLoading } = useLoading();
 
-	const execute = async (...args: any[]) => {
-		try {
-			setIsLoading(true);
-			const result = await func(...args);
-			return result;
-		} finally {
-			setIsLoading(false);
-		}
-	};
+	const execute = useCallback(
+		async (...args: Args): Promise<T> => {
+			try {
+				setIsLoading(true);
+				const result = await func(...args);
+				return result;
+			} catch (error) {
+				throw new Error(
+					`予期せぬエラーが発生しました。${error instanceof Error ? error.message : JSON.stringify(error)}`,
+					{ cause: error },
+				);
+			} finally {
+				setIsLoading(false);
+			}
+		},
+		[func, setIsLoading],
+	);
 
 	return execute;
 };
